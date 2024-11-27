7175a5a059b2d80458b197018299ec18de90d385
rohithti <rohithti@amazon.com>
2024-11-27 10:36:07 +0530

    Add a basic UI
    Includes full functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [formData, setFormData] = useState({
+    url: "",
+  });
+
+  const [metaData, setMetaData] = useState({
+    errors: {},
+    loading: false,
+  })
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setMetaData({
+      ...metaData,
+      loading: true,
+    });
+
+
+    if (validateForm()) {
+      console.log(formData);
+      fetch('http://localhost:8080/v1/url', {
+        method: 'POST',
+        body: JSON.stringify(formData)
+      }).then(res => res.json().then(r => console.log("response received: " + JSON.stringify(r))))
+          .catch(e => {
+            console.log("Error occurred: " + e.message)
+          })
+    } else {
+      console.log("Form is invalid");
+    }
+
+    setMetaData({
+      ...metaData,
+      loading: false,
+    });
+  }
+  
+  const validateForm = () => {
+    const errors = {};
+
+    if (!formData.url) {
+      errors.url = "url cannot be empty";
+    }
+
+    setMetaData({ ...metaData, errors: errors });
+    console.log(metaData.errors.url)
+
+    return Object.keys(errors).length === 0;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+        <label>
+          Url:
+          <input type="text" name="url" placeholder="Url" value={formData.url} onChange={handleChange} />
+        </label> {metaData.errors.url && (
+            <p style={{ color: "red" }}>{metaData.errors.url}</p>
+        )}
+        <input type="submit" value="Submit" />
+        </form>
+      </div>
   );
 }
 
